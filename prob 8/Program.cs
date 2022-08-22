6f12b406f25dc6ab17449d9bd6e6b24cf3164087
username <username@cognizant.com>
2022-08-22 21:01:18 +0530

    Add project files.

---
+++ prob 8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable numberNames = new Hashtable();
+            numberNames.Add(1, "Seven");
+            numberNames.Add(2, "Eight");
+            numberNames.Add(3, "Nine");
+            foreach(DictionaryEntry de in numberNames)
+                Console.WriteLine("Key: {0}, Value: {1}", de.Key, de.Value);
+            Console.ReadLine();
+        }
+    }
+}
